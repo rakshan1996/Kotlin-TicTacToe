3cf0616556a3a378cf8283806da9c3da711a58af
Rakshan Thapar <rakshanthapar32@gmail.com>
2018-08-07 00:54:27 +0530

    Added Buttons that Prompt whether i have selected Single Player or MultiPlayer

--- app/src/main/java/com/example/devilspc/tictactoe/Landing.kt
+++ app/src/main/java/com/example/devilspc/tictactoe/Landing.kt
@@ -2,11 +2,31 @@ package com.example.devilspc.tictactoe
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import java.nio.file.attribute.AttributeView
 
 class Landing : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+    }
+
+    fun GetName(view: View){
+
+        var BuId=view as Button
+        when(BuId.id)
+        {
+            R.id.SinglePlayer -> {
+                Toast.makeText(this,"Single Player Game Selected",Toast.LENGTH_SHORT).show()
+            }
+
+            R.id.MultiPlayer -> {
+                Toast.makeText(this,"Multi Player Game Selected",Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
